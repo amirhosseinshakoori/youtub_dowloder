a79e6d5c299ecec4da08d24458061c63c1e84cdf
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-10-03 19:06:49 +0330

    chenge stting

--- youtubdl/youtubdl/settings.py
+++ youtubdl/youtubdl/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "core",
+    'pytube',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
