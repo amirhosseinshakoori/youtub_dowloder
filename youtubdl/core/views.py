560cf718c767198ed34b141b6dc186391cf6c2d8
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-10-03 19:27:13 +0330

    add downloadlink view

--- youtubdl/core/views.py
+++ youtubdl/core/views.py
@@ -1,4 +1,23 @@
+import pytube
 from django.shortcuts import render
+from pytube import YouTube
+from django.http import HttpResponseRedirect
+from . forms import SaveUrloathform
+
 
 def index(request):
     return render (request, 'core/index.html')
+
+
+def downloadLink(request):
+    if request.method == 'POST'
+        form=SaveUrloathform(request.Post)
+        if form.is_valid():
+            link= form.cleaned_data['link']
+            if link:
+                yt=pytube.YouTube(link)
+                yt.streams.first.download()
+                return HttpResponseRedirect('/')
+        else:
+            form = SaveUrloathform()
+        return render(request,'core/new_download.html',{'form':form})    
